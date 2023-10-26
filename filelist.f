c7ce43e1ed2a400fd4246e2c2de258e52d752ba4
Alejandro Tafalla <atafalla@bsc.es>
2023-10-26 10:47:53 +0200

    Remove PLIC, CLINT and riscv_peripherals
    It will be instantiated in OpenPiton
    
    (cherry picked from commit 4b52be310d1789b0a5d4ee78483ffe04e4343aae)

--- filelist.f
+++ filelist.f
@@ -1,14 +1,9 @@
-../ariane/corev_apu/riscv-dbg/src/dm_pkg.sv
-../ariane/corev_apu/axi/src/axi_pkg.sv
-./includes/ariane_axi_pkg.sv
-
 -F ./rtl/core/sargantana/filelist.f
 ./rtl/dcache/rtl/src/target/generic/hpdcache_params_pkg.sv
 -f ./rtl/dcache/rtl/hpdcache.Flist
 -F ./rtl/icache/filelist.f
 -F ./rtl/mmu/filelist.f
 +incdir+./includes
-+incdir+../ariane/corev_apu/register_interface/include/
 ./includes/sargantana_hpdc_pkg.sv
 ./includes/wt_cache_pkg.sv
 
@@ -28,27 +23,3 @@
 ./rtl/dcache/rtl/src/target/cinco-ranch/hpdcache_subsystem_l15_adapter.sv
 ./rtl/drac_openpiton_wrapper.sv
 
-./rtl/riscv_peripherals.sv
-./rtl/rv_plic/rtl/rv_plic_target.sv
-./rtl/rv_plic/rtl/rv_plic_gateway.sv
-./rtl/rv_plic/rtl/plic_regmap.sv
-./rtl/rv_plic/rtl/plic_top.sv
-
-./rtl/clint.sv
-../ariane/corev_apu/clint/axi_lite_interface.sv
-../ariane/core/include/axi_intf.sv
-../ariane/corev_apu/axi_mem_if/src/axi2mem.sv
-../ariane/common/submodules/common_cells/src/cdc_2phase.sv
-../ariane/corev_apu/riscv-dbg/debug_rom/debug_rom.sv
-../ariane/corev_apu/riscv-dbg/src/dm_csrs.sv
-../ariane/corev_apu/riscv-dbg/src/dm_mem.sv
-../ariane/corev_apu/riscv-dbg/src/dm_top.sv
-../ariane/corev_apu/riscv-dbg/src/dmi_cdc.sv
-../ariane/corev_apu/riscv-dbg/src/dmi_jtag.sv
-../ariane/corev_apu/riscv-dbg/src/dm_sba.sv
-../ariane/corev_apu/riscv-dbg/src/dmi_jtag_tap.sv
-../ariane/corev_apu/src/tech_cells_generic/src/deprecated/cluster_clk_cells.sv
-../ariane/corev_apu/src/tech_cells_generic/src/deprecated/pulp_clk_cells.sv
-../ariane/corev_apu/src/tech_cells_generic/src/rtl/tc_clk.sv
-../ariane/common/submodules/common_cells/src/deprecated/fifo_v2.sv
-../ariane/corev_apu/riscv-dbg/debug_rom/debug_rom_one_scratch.sv
